146c17e8e6fa927f95fa5ba8caf3ae23a2529850
Ovidio Perez <oapm10@gmail.com>
2021-12-22 21:18:11 -0400

    Refactor de components to use the data provided

--- js/components/grid-item.js
+++ js/components/grid-item.js
@@ -1,15 +1,21 @@
 export default {
+    props: {
+        project: {
+            type: Object,
+            required: true,
+        },
+    },
     template: `
     <div class="col-lg-4 col-md-6 grid-item mb-4">
         <div class="listing-item">
             <div class="position-relative">
                 <ul class="list-inline listing-tags m-0">
                     <li class="list-inline-item">
-                        <router-link class="reset-anchor font-weight-normal text-gray text-small" to="/details">Creative</router-link>
+                        <router-link class="reset-anchor font-weight-normal text-gray text-small" to="/works">Creative</router-link>
                     </li>
                 </ul>
-                <router-link class="reset-anchor d-block listing-img-holder" to="/details">
-                    <img class="img-fluid" src="img/portfolio-3.jpg" alt="">
+                <router-link class="reset-anchor d-block listing-img-holder" to="/works">
+                    <img class="img-fluid" :src="project.thumbnail" alt="">
                     <p class="mb-0 text-primary small d-flex align-items-center listing-btn"> <span>Look inside</span>
                         <svg class="svg-icon text-primary svg-icon-sm ml-2">
                         <use xlink:href="#arrow-right-1"> </use>
@@ -18,11 +24,10 @@ export default {
                 </router-link>
             </div>
             <div class="py-3">
-                <router-link class="reset-anchor" to="/details">
-                    <h2 class="h5 listing-item-heading">Tour de France</h2>
+                <router-link class="reset-anchor" to="/works">
+                    <h2 class="h5 listing-item-heading">{{project.title}}</h2>
                 </router-link>
-                <p class="text-small mb-0 listing-item-description">Lorem ipsum dolor sit amet, consectetur adipisicing
-                elit</p>
+                <p class="text-small mb-0 listing-item-description">{{project.short_description}}</p>
             </div>
         </div>
     </div>
--- js/components/masonry-wrapper.js
+++ js/components/masonry-wrapper.js
@@ -1,34 +1,51 @@
 import gridItem from "../components/grid-item.js";
+import fetchData from "../fetchData.js";
 
 export default {
     template: `
     <div class="masonry-wrapper">
-        <grid-item></grid-item>
-        <grid-item></grid-item>
-        <grid-item></grid-item>
-        <grid-item></grid-item>
-        <grid-item></grid-item>
-        <grid-item></grid-item>
+        <grid-item 
+            v-for="project in projects"
+            v-bind:key="project.id"
+            v-bind:project="project"
+        ></grid-item>
     </div>
     `,
+    data: () => ({
+        projects: [],
+    }),
     components: {
         gridItem,
     },
     methods: {
         masonryDistribution: function () {
-            var $grid = $(".masonry-wrapper").masonry({
-                itemSelector: ".grid-item",
-                columnWidth: ".grid-item",
-                percentPosition: true,
-                transitionDuration: 300,
-            });
-
-            $grid.imagesLoaded().progress(function () {
-                $grid.masonry();
+            this.$nextTick(function () {
+                var $grid = $(".masonry-wrapper").masonry({
+                    itemSelector: ".grid-item",
+                    columnWidth: ".grid-item",
+                    percentPosition: true,
+                    transitionDuration: 300,
+                });
+                $grid.imagesLoaded().progress(function () {
+                    $grid.masonry();
+                });
             });
         },
+        fetchProjects: function () {
+            fetch("../../data/projects.json")
+                .then((data) => data.json())
+                .then((projectsJson) => {
+                    this.projects = [...projectsJson.projects];
+                })
+                .catch((err) => {
+                    console.error(err);
+                });
+        },
     },
-    mounted: function () {
+    updated: function () {
         this.masonryDistribution();
+    },
+    created: function () {
+        this.fetchProjects();
     },
 };
