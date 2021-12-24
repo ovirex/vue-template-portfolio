78258cfbf4f157500b97eb2925c16bb9d4b30d53
Ovidio Perez <oapm10@gmail.com>
2021-12-24 03:35:41 -0400

    Make the component to render the projects according to the path

--- js/components/masonry-wrapper.js
+++ js/components/masonry-wrapper.js
@@ -4,7 +4,7 @@ export default {
     template: `
     <div class="masonry-wrapper">
         <grid-item 
-            v-for="project in projects"
+            v-for="project in projectsToShow"
             v-bind:key="project.id"
             v-bind:project="project"
         ></grid-item>
@@ -12,26 +12,33 @@ export default {
     `,
     data: () => ({
         projects: [],
+        projectsToShow: [],
+        path: "",
     }),
     components: {
         gridItem,
     },
     methods: {
-        masonryDistribution: function () {
+        masonryDistribution: function (reload = false) {
             this.$nextTick(function () {
-                var $grid = $(".masonry-wrapper").masonry({
-                    itemSelector: ".grid-item",
-                    columnWidth: ".grid-item",
-                    percentPosition: true,
-                    transitionDuration: 300,
-                });
-                $grid.imagesLoaded().progress(function () {
-                    $grid.masonry();
-                });
+                var $grid = $(".masonry-wrapper");
+                if (!reload) {
+                    $grid.masonry({
+                        itemSelector: ".grid-item",
+                        columnWidth: ".grid-item",
+                        percentPosition: true,
+                        transitionDuration: 300,
+                    });
+                    $grid.imagesLoaded().progress(function () {
+                        $grid.masonry();
+                    });
+                } else {
+                    $grid.masonry("reloadItems");
+                }
             });
         },
-        fetchProjects: function () {
-            fetch("../../data/projects.json")
+        fetchProjects: async function () {
+            await fetch("../../data/projects.json")
                 .then((data) => data.json())
                 .then((projectsJson) => {
                     this.projects = [...projectsJson.projects];
@@ -40,11 +47,50 @@ export default {
                     console.error(err);
                 });
         },
+        filterProjectsAccordingToPath: async function () {
+            await this.fetchProjects();
+
+            this.path = this.$route.path.replace("/", "");
+
+            let tag = this.capitalizePathName(this.path);
+
+            if (tag === null) {
+                this.projectsToShow = this.projects;
+            } else {
+                this.projectsToShow = this.projects.filter((project) => {
+                    return project.tags.includes(tag);
+                });
+            }
+            this.masonryDistribution();
+        },
+        capitalizePathName: function (path) {
+            /**
+             * This function make the current path to have a format similar to the
+             * Strings in the tags Array of the projects.json
+             */
+            if (path === "") {
+                return null;
+            }
+            return path
+                .split("-")
+                .map((word) => {
+                    return `${word[0].toUpperCase() + word.slice(1)}`;
+                })
+                .join(" ");
+        },
     },
     updated: function () {
-        this.masonryDistribution();
+        this.masonryDistribution(true);
     },
     created: function () {
-        this.fetchProjects();
+        this.filterProjectsAccordingToPath();
+    },
+
+    watch: {
+        $route(to, from) {
+            // react to route changes...
+            this.path = to.path.replace("/", "");
+            this.filterProjectsAccordingToPath();
+        },
     },
 };
